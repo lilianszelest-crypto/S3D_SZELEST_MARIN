95a3b4a5e23226f1f46c2366d0291f62ee3db778
Clément Marin <clement.marin5@etu.univ-lorraine.fr>
2026-09-22 16:48:15 +0200

    ajout de la classe TestEtudiant

--- EXO_3/Tests/TestEtudiant.java
+++ EXO_3/Tests/TestEtudiant.java
@@ -1,2 +1,52 @@
+import org.junit.Test;
+
 public class TestEtudiant {
+
+    @Test
+    public void testAjouterNoteValides(){
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathématiques");
+        formation.ajouterMatiere(maths , 3);
+        Identite identite = new Identite("12345" , "Dupont" , "Jean");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(math,15);
+        assertEquals(15.0, etudiant.calculerMoyenne(math), 0.01);
+    }
+
+    @Test
+    public void testAjouterNoteHorsIntervalle() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        formation.ajouterMatiere(maths, 3);
+        Identite identite = new Identite("12345", "Dupont", "Jean");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(maths, 25);
+        assertEquals(-1.0, etudiant.calculerMoyenne(maths), 0.01);
+    }
+
+    @Test
+    public void testAjouterNoteMatiereHorsFormation() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        Matiere musique = new Matiere("Musique");
+        formation.ajouterMatiere(maths, 3);
+        Identite identite = new Identite("12345", "Dupont", "Jean");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(musique, 15);
+        assertEquals(-1.0, etudiant.calculerMoyenne(musique), 0.01);
+    }
+
+    @Test
+    public void testMoyenneGenerale() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        Matiere ArchiReseaux = new Matiere("ArchiReseaux");
+        formation.ajouterMatiere(maths, 2);
+        formation.ajouterMatiere(ArchiReseaux, 1);
+        Identite identite = new Identite("12345", "Dupont", "Jean");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(maths, 10);
+        etudiant.ajouterNote(info, 16);
+        assertEquals(12.0, etudiant.calculerMoyenneGenerale(), 0.01);
+    }
 }
