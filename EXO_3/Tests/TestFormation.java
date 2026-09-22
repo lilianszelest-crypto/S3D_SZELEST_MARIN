bdad035e8ce7161229efb3ebc5078ee5ccce77ee
SZELEST Lilian <lilian.szelest@gmail.com>
2026-09-22 17:06:22 +0200

    ajout test Formation

--- EXO_3/Tests/TestFormation.java
+++ EXO_3/Tests/TestFormation.java
@@ -1,2 +1,38 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
 public class TestFormation {
+
+    @Test
+    public void testAjoutMatiere(){
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        formation.ajouterMatiere(maths, 3);
+        assertTrue(formation.contientMatiere(maths));
+        assertEquals(3, formation.getCoefficient(maths));
+    }
+    @Test
+    public void testSupprMatiere(){
+        Formation formation = new Formation("BUT1");
+        Matiere Qdev = new Matiere("QualiteDev");
+        formation.ajouterMatiere(Qdev, 67);
+        formation.supprimerMatiere(Qdev);
+        assertFalse(formation.contientMatiere(Qdev));
+    }
+
+    @Test
+    public void testCoefMatPasLa(){
+        Formation formation = new Formation("BUT1");
+        Matiere WEB = new Matiere("DevWeb");
+        assertEquals(-1, formation.getCoefficient(WEB));
+
+    }
+
+
+
+
+
 }
