8da59a3cb35ff9c96732f8615a5393f75103efc2
Clément Marin <clement.marin5@etu.univ-lorraine.fr>
2026-09-22 17:16:07 +0200

    Ajout de tests

--- EXO_3/Tests/TestEtudiant.java
+++ EXO_3/Tests/TestEtudiant.java
@@ -45,10 +45,53 @@ public class TestEtudiant {
         Matiere ArchiReseaux = new Matiere("ArchiReseaux");
         formation.ajouterMatiere(maths, 2);
         formation.ajouterMatiere(ArchiReseaux, 1);
-        Identite identite = new Identite("12345", "Dupont", "Jean");
+        Identite identite = new Identite("12345", "Dupont", "Lola");
         Etudiant etudiant = new Etudiant(identite, formation);
         etudiant.ajouterNote(maths, 10);
         etudiant.ajouterNote(ArchiReseaux, 16);
         assertEquals(12.0, etudiant.calculerMoyenneGenerale(), 0.01);
+    }
+
+    @Test
+    public void testMoyenneGeneraleAvecTroisCoefficients() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        Matiere info = new Matiere("Informatique");
+        Matiere anglais = new Matiere("Anglais");
+        formation.ajouterMatiere(maths, 3);
+        formation.ajouterMatiere(info, 2);
+        formation.ajouterMatiere(anglais, 1);
+        Identite identite = new Identite("12345", "Dupont", "Minas");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(maths, 12);
+        etudiant.ajouterNote(info, 8);
+        etudiant.ajouterNote(anglais, 18);
+        assertEquals(70.0 / 6, etudiant.calculerMoyenneGenerale(), 0.01);
+    }
+
+    @Test
+    public void testMoyenneGeneraleSansNote() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        formation.ajouterMatiere(maths, 3);
+        Identite identite = new Identite("12345", "Dupont", "Maxime");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        assertEquals(-1.0, etudiant.calculerMoyenneGenerale(), 0.01);
+    }
+
+    @Test
+    public void testMoyenneGeneraleMatiereSansNoteIgnoree() {
+        Formation formation = new Formation("BUT1");
+        Matiere maths = new Matiere("Mathematiques");
+        Matiere info = new Matiere("Informatique");
+        Matiere anglais = new Matiere("Anglais");
+        formation.ajouterMatiere(maths, 3);
+        formation.ajouterMatiere(info, 2);
+        formation.ajouterMatiere(anglais, 1);
+        Identite identite = new Identite("12345", "Dupont", "lilian");
+        Etudiant etudiant = new Etudiant(identite, formation);
+        etudiant.ajouterNote(maths, 10);
+        etudiant.ajouterNote(info, 14);
+        assertEquals(58.0 / 5, etudiant.calculerMoyenneGenerale(), 0.01);
     }
 }
